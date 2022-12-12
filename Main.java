94d78f5bc9bc1f6e0f9df78e1edfdbc1280d31b0
Jgaray3008 <110505188+Jgaray3008@users.noreply.github.com>
2022-12-11 19:23:18 -0600

    Add files via upload
    Commit Joseph

--- Main.java
+++ Main.java
@@ -19,7 +19,8 @@ public class Main {
                 *       Arleth Serrano Rodriguez   *                             
                 ************************************
                 """);
-        System.out.print("\033[34mPresione 1 para empezar el juego");
+        System.out.print("\033[34mDigite 1 para empezar el juego o cualquier tecla para salir.\n-> ");
+
         star = entrada2.nextInt();
         switch (star) {
             case 1 -> Start();
@@ -48,7 +49,7 @@ public class Main {
                 if (Posicion == agua) {
                     System.out.print(agua + " ");
                 } else {
-                    System.out.print(Posicion + "  ");
+                    System.out.print(Posicion + " ");
                 }
             }
             System.out.println();
@@ -59,146 +60,138 @@ public class Main {
     private static void Start() {
         tableros tablero = new tableros();
         String[] nombres = new String[4];
+        int Aleatorios[] = new int[6];
 
         char[][] TuTablero = tablero.crearTablero(tablero.gettamanoTablero(), tablero.getagua());
         char[][] TableroEnemigo = tablero.crearTablero(tablero.gettamanoTablero(), tablero.getagua());
+        char[][] TableroenemigoAtacado = tablero.crearTablero(tablero.gettamanoTablero(), tablero.getagua());
+
         char agua = tablero.getagua();
+
+        Stack<Integer> Numeros = new Stack<Integer>();
+        Random Rnd = new Random();
+        int Aleatorio;
+        for (int x = 0; x < 6; x++) {
+            Aleatorio = (int) (Rnd.nextDouble() * 6);
+            while (Numeros.contains(Aleatorio)) {
+                Aleatorio = (int) (Rnd.nextDouble() * 6);
+                //Posiciones de las naves enemigas
+            }
+            Numeros.push(Aleatorio);
+            Aleatorios[x] = Aleatorio;
+        }
+        System.out.println("\n");
+
+        TableroEnemigo[Aleatorios[0]][Aleatorios[1]] = '1';
+        TableroEnemigo[Aleatorios[2]][Aleatorios[3]] = '2';
+        TableroEnemigo[Aleatorios[4]][Aleatorios[5]] = '3';
 
         int Almirante = 4;
         int Capitan = 3;
         int Teniente = 4;
         Scanner entrada = new Scanner(System.in);
         int menu;
+
         boolean salir = true;
+
+
+        System.out.println("Ingresa los nombres de los jugadores: ");
+        // Se piden los nombres a los jugadores
+        Scanner entrada3 = new Scanner(System.in);
+        Scanner entrada4 = new Scanner(System.in);
+        System.out.print("\033[34mNombre del jugador 1 \uD83D\uDC64= ");
+        String jugador1 = entrada3.nextLine();
+        System.out.print("Nombre del jugador 2 \uD83D\uDC64= ");
+        String jugador2 = entrada4.nextLine();
+        System.out.println("----" + jugador1 + "----");
+        ImprimirTablero(TuTablero, agua);
+        nombres[0] = jugador1;
+        nombres[1] = jugador2;
+
+        System.out.println("\nIngrese donde quiere colocar sus naves Capitán.");
+        Scanner s = new Scanner(System.in);
+        int col;
+        int fila;
+
+        System.out.println("\nIngrese donde quiere colocar la nave del almirante, Capitan");
+        System.out.print("Fila: ");
+        fila = s.nextInt();
+        fila = fila-1;
+        System.out.print("Columna: ");
+        col = s.nextInt();
+        col = col-1;
+        // Busca en el tablero y coloca la nave
+        TuTablero[fila][col] = '1';
+        System.out.println("----" + nombres[0] + "----");
+        ImprimirTablero(TuTablero, agua);
+
+        Scanner c = new Scanner(System.in);
+        int colc;
+        int filac;
+
+        System.out.println("\nIngrese donde quiere colocar su nave Capitan");
+        System.out.print("Fila: ");
+        filac = c.nextInt();
+        filac = filac-1;
+        System.out.print("Columna: ");
+        colc = c.nextInt();
+        colc = colc-1;
+        // Busca en el tablero y coloca la nave
+        TuTablero[filac][colc] = '2';
+        System.out.println("----" + nombres[0] + "----");
+        ImprimirTablero(TuTablero, agua);
+
+        Scanner t = new Scanner(System.in);
+        int colt;
+        int filat;
+        System.out.println("\nIngrese donde quiere colocar la nave del teniente Capitan");
+        System.out.print("Fila: ");
+        filat = t.nextInt();
+        filat = filat-1;
+        System.out.print("Columna: ");
+        colt = t.nextInt();
+        colt = colt-1;
+        // Busca en el tablero y coloca la nave
+        TuTablero[filat][colt] = '3';
+        System.out.println("----" + nombres[0] + "----");
+        ImprimirTablero(TuTablero, agua);
+
+
         while (salir) {
-            System.out.println("1 -Ingresar nombres");
-            System.out.println("2 -Ver tablero propio");
-            System.out.println("3 -Ver tablero de ataque");
-            System.out.println("4 -Colocar naves del capitán");
-            System.out.println("5 -Atacar");
-            System.out.println("6 -Hacer trampa");
-            System.out.println("6 -Exit");
+
+            System.out.println("\n\n1 -Ver tableros");
+            System.out.println("2 -Atacar");
+            System.out.println("3 -Hacer trampa");
+            System.out.println("4 -Exit");
+            System.out.print("-> ");
             menu = entrada.nextInt();
             switch (menu) {
                 case 1:
-                    System.out.println("\n1 Ingresar los nombres de los jugadores ");
-                    // Se piden los nombres a los jugadores
-                    Scanner entrada3 = new Scanner(System.in);
-                    Scanner entrada4 = new Scanner(System.in);
-                    System.out.print("\033[34mNombre del jugador 1 \uD83D\uDC64= ");
-                    String jugador1 = entrada3.nextLine();
-                    System.out.print("Nombre del jugador 2 \uD83D\uDC64= ");
-                    String jugador2 = entrada4.nextLine();
-                    System.out.println("----" + jugador2 + "----");
-                    ImprimirTablero(TableroEnemigo, agua);
-                    nombres[0] = jugador1;
-                    nombres[1] = jugador2;
+                    System.out.print("\n*******************************");
+                    System.out.println("\n----" + nombres[1] + "----");
+                    ImprimirTablero(TableroenemigoAtacado, agua);
+
+                    System.out.println("\n----" + nombres[0] + "----");
+                    ImprimirTablero(TuTablero, agua);
+                    System.out.print("*******************************\n");
+
                     break;
                 case 2:
-                    System.out.println("\n2 Ver tablero propio");
-                    System.out.println("----" + nombres[0] + "----");
-                    ImprimirTablero(TuTablero, agua);
-
+                    System.out.println("\n5 Atacar");
                     break;
                 case 3:
-                    System.out.println("\n3 Ver tablero de ataque");
-                    System.out.println("----" + nombres[1] + "----");
+                    System.out.println("\n\n*****Haciendo trampa*****");
                     ImprimirTablero(TableroEnemigo, agua);
+                    System.out.println("*****Haciendo trampa*****\n\n");
+
                     break;
                 case 4:
-                    System.out.println("Ingrese donde quiere colocar sus naves Capitán solo una vez por partida");
-                    Scanner s = new Scanner(System.in);
-                    int col;
-                    int fila;
-                    System.out.println("                                             ");
-                    System.out.println("Ingrese donde quiere colocar la nave del almirante, Capitan");
-                    System.out.println("Fila:");
-                    fila = s.nextInt();
-                    System.out.println("Columna");
-                    col = s.nextInt();
-                    // Busca en el tablero y coloca la nave
-                    TuTablero[fila][col] = '1';
-                    System.out.println("----" + nombres[0] + "----");
-                    for (int i = 0; i < 6; i++) {
-                        for (int j = 0; j < 6; j++) {
-                            System.out.print(TuTablero[i][j]);
-                            System.out.print(" ");
-                        }
-                        System.out.println(" ");
-                    }
-
-                    Scanner c = new Scanner(System.in);
-                    int colc;
-                    int filac;
-                    System.out.println("                                             ");
-                    System.out.println("Ingrese donde quiere colocar su nave Capitan");
-                    System.out.println("Fila:");
-                    filac = c.nextInt();
-                    System.out.println("Columna");
-                    colc = c.nextInt();
-                    // Busca en el tablero y coloca la nave
-                    TuTablero[filac][colc] = '2';
-                    System.out.println("----" + nombres[0] + "----");
-                    for (int i = 0; i < 6; i++) {
-                        for (int j = 0; j < 6; j++) {
-                            System.out.print(TuTablero[i][j]);
-                            System.out.print(" ");
-                        }
-                        System.out.println(" ");
-                    }
-
-                    Scanner t = new Scanner(System.in);
-                    int colt;
-                    int filat;
-                    System.out.println("                                             ");
-                    System.out.println("Ingrese donde quiere colocar la nave del teniente Capitan");
-                    System.out.println("Fila:");
-                    filat = t.nextInt();
-                    System.out.println("Columna");
-                    colt = t.nextInt();
-                    // Busca en el tablero y coloca la nave
-                    TuTablero[filat][colt] = '3';
-                    System.out.println("----" + nombres[0] + "----");
-                    for (int i = 0; i < 6; i++) {
-                        for (int j = 0; j < 6; j++) {
-                            System.out.print(TuTablero[i][j]);
-                            System.out.print(" ");
-                        }
-                        System.out.println(" ");
-                    }
+                    System.out.println("\nSalida del juego!");
+                    salir = false;
                     break;
-                case 5:
-                    System.out.println("\n4 Atacar");
-                    System.out.println("\n3 Ver tablero de ataque");
-                    System.out.println("----" + nombres[1] + "----");
-                    ImprimirTablero(TableroEnemigo, agua);
-                    System.out.println("Naves de enemigo colocadas...");
-                    Stack <Integer>Numeros=new Stack<Integer>();
-                    Random Rnd = new Random();
-                    int Aleatorio;
-                    int Aleatorios[]=new int [5];
-                    for (int i=0;i<5;i++){
-                        Aleatorio=(int)(Rnd.nextDouble()*5+1);
-                        while (Numeros.contains(Aleatorio)){
-                            Aleatorio=(int)(Rnd.nextDouble()*5+1);
-                        }
-                        Numeros.push(Aleatorio);
-                        Aleatorios[i]=Aleatorio;
-                        System.out.println(Aleatorios[i]);
-                    }
-
+                default:
+                    System.out.println("\n *Error: Ingrese una opción válida");
                     break;
-
-                case 6:
-                            System.out.println("\n5 Hacer trampa");
-                            break;
-                        case 7:
-                            System.out.println("\nSalida del juego!");
-                            salir = false;
-                            break;
-                        default:
-                            System.out.println("\n *Error: Ingrese una opción válida");
-                            break;
                     }
             }
         }
